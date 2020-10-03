0c520c322f2a2b5b612f16f2d61c5986103e3f46
Dinesh Balaji Venkataraj <www.sidthesloth@gmail.com>
2020-10-03 21:10:29 +0530

    build: add tailwind to the project

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
+  ]
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  purge: {
+    content: ['./src/**/*.{js,ts,jsx,tsx}']
+  },
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
