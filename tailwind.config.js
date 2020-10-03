b6635ff5a78f4df215565113b3bb146fb1e72ded
Dinesh Balaji Venkataraj <www.sidthesloth@gmail.com>
2020-10-04 03:01:29 +0530

    refactor(styling): configure tailwind theme with colors, spacing and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -7,6 +10,39 @@ module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx}']
   },
   theme: {
+    colors: {
+      black: '#000',
+      white: '#fff',
+      primary: {
+        shade: 'var(--color-primary-shade)',
+        default: 'var(--color-primary)',
+        tint: 'var(--color-primary-tint)',
+        light: 'var(--color-primary-light)'
+      },
+      secondary: {
+        shade: 'var(--color-secondary-shade)',
+        default: 'var(--color-secondary)',
+        tint: 'var(--color-secondary-tint)',
+        light: 'var(--color-secondary-light)'
+      },
+      dark: {
+        shade: 'var(--color-dark-shade)',
+        default: 'var(--color-dark)',
+        tint: 'var(--color-dark-tint)',
+        light: 'var(--color-dark-light)'
+      }
+    },
+    spacing: {
+      8: '8px',
+      16: '16px',
+      32: '32px',
+      64: '64px'
+    },
+    fontFamily: {
+      sans: ['Nunito\\ Sans', ...defaultTheme.fontFamily.sans],
+      cursive: ['Adinda\\ Melia'],
+      mono: [...defaultTheme.fontFamily.mono]
+    },
     extend: {}
   },
   variants: {},
