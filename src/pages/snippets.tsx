3a4c4d97f45918bce02fbcec644a86dd9b0d44e2
Dinesh Balaji Venkataraj <www.sidthesloth@gmail.com>
2020-10-07 00:00:19 +0530

    feat(component): add the snippets page of the application

---
+++ src/pages/snippets.tsx
@@ -0,0 +1,191 @@
+import PageBody from '../components/page-body/PageBody';
+import PageHeader from '../components/page-header/PageHeader';
+import Page from '../components/page/Page';
+
+/**
+ * The snippets page of the application.
+ */
+const SnippetsPage: React.FC = () => {
+  return (
+    <Page>
+      <PageHeader
+        title="Snippets"
+        description="A collection of copy paste times that do some standalone work on their
+        own."
+      />
+      <PageBody>
+        <div className="table w-full">
+          <thead>
+            <tr className="text-primary">
+              <th className="w-3/12 p-4 pb-8 border-b border-dark-light">
+                Name
+              </th>
+              <th className="w-5/12 p-4 pb-8 border-b border-dark-light">
+                Category
+              </th>
+              <th className="w-2/12 p-4 pb-8 border-b border-dark-light">
+                Updated
+              </th>
+              <th className="w-2/12 p-4 pb-8 border-b border-dark-light">
+                Description
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4 border-b border-dark-light">
+                Date to JSON
+              </td>
+              <td className="w-5/12 p-4 border-b border-dark-light">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                JSON Typescript
+              </td>
+              <td className="w-2/12 p-4 border-b border-dark-light text-center">
+                May 29, 2017
+              </td>
+            </tr>
+            <tr>
+              <td className="w-3/12 p-4">Date to JSON</td>
+              <td className="w-5/12 p-4">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do{' '}
+              </td>
+              <td className="w-2/12 p-4 text-center">JSON Typescript</td>
+              <td className="w-2/12 p-4 text-center">May 29, 2017</td>
+            </tr>
+          </tbody>
+        </div>
+      </PageBody>
+    </Page>
+  );
+};
+
+export default SnippetsPage;
