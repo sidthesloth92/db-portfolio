69691675aa33499ed6c402288e195296928d34a4
Dinesh Balaji Venkataraj <www.sidthesloth@gmail.com>
2020-10-17 01:22:55 +0530

    refactor(page): reduced page top padding

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -3,7 +3,7 @@
  * Projects the children as is inside the page.
  */
 const Page: React.FC = ({ children }) => (
-  <div className="max-w-screen-lg pt-20 pb-40 px-4 m-auto">{children}</div>
+  <div className="max-w-screen-lg pt-12 pb-40 px-4 m-auto">{children}</div>
 );
 
 export default Page;
