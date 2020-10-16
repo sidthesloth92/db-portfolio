e313c683ae3633593002d7207bf9d298184994f6
Dinesh Balaji Venkataraj <www.sidthesloth@gmail.com>
2020-10-17 03:07:22 +0530

    fix(global): fix issue where mobile page where horizonally scrollable

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -3,7 +3,9 @@
  * Projects the children as is inside the page.
  */
 const Page: React.FC = ({ children }) => (
-  <div className="max-w-screen-lg pt-12 pb-40 px-4 m-auto">{children}</div>
+  <div className="max-w-screen-lg pt-12 pb-40 px-4 m-auto overflow-hidden">
+    {children}
+  </div>
 );
 
 export default Page;
