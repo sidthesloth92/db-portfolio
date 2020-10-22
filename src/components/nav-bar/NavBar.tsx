61de8ca43a2cf11f112da66a2304d3cdced3d36b
Dinesh Balaji Venkataraj <www.sidthesloth@gmail.com>
2020-10-22 18:59:49 +0530

    feat(navbar): add shadow to nav bar

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar: React.FC = () => {
   return (
     <>
       {router.pathname.length > 1 && (
-        <header className="fixed top-0 left-0 w-full h-20 flex justify-center d z-10">
+        <header className="nav-bar fixed top-0 left-0 w-full h-20 flex justify-center z-10 bg-dark">
           <div className="flex w-full max-w-screen-lg h-full justify-between items-center px-8">
             <Link href="/">
               <a className="font-cursive text-3xl text-primary">
@@ -20,6 +20,14 @@ const NavBar: React.FC = () => {
           </div>
         </header>
       )}
+
+      <style jsx>
+        {`
+          .nav-bar {
+            box-shadow: 0px 0px 30px 25px rgba(32, 37, 44, 1);
+          }
+        `}
+      </style>
     </>
   );
 };
