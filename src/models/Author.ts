3513ab6d417999aad9a07b8fc8f3b4b2a78684c2
Dinesh Balaji Venkataraj <dvenkataraj@nvenergy.com>
2020-10-26 17:56:50 +0530

    feat(posts): load posts initally from dev.to

---
+++ src/models/Author.ts
@@ -0,0 +1,12 @@
+/**
+ * Author of a {@link Post}.
+ */
+export interface Author {
+  name: string;
+  username: string;
+  twitter_username: string;
+  github_username: string;
+  website_url: string;
+  profile_image: string;
+  profile_image_90: string;
+}
