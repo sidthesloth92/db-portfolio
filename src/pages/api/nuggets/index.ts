df962727498eb6165a040eeb0589538a89fd1819
Dinesh Balaji Venkataraj <dvenkataraj@nvenergy.com>
2020-11-01 19:22:33 +0530

    fix(nuggets): fix nuggets of other users being fetched

--- src/pages/api/nuggets/index.ts
+++ src/pages/api/nuggets/index.ts
@@ -52,29 +52,7 @@ export async function getNuggets({
   per_page = process.env.NUGGETS_PER_PAGE,
   tag = ''
 }: GetNuggetsProps = {}): Promise<Nugget[]> {
-  if (tag) {
-    return await getNuggetsWithTag({
-      page,
-      per_page,
-      tag
-    });
-  }
-
-  return await getNuggetsWithoutTag({
-    page,
-    per_page
-  });
-}
-
-/**
- * Fetches a list of nuggets with specified tag. Since DEV.to does not have
- * and endpoint that allows to filter by username and tag, instead we fetch a
- * fairly large number of nuggets of the user and then try to filter out the nuggets
- * matching the tag and in addition to nuggest as a tag.
- * @param param of type {@link GetNuggetsProps}.
- */
-const getNuggetsWithTag = async ({ page, per_page, tag }) => {
-  console.log('Getting nuggets with tag: ', page, per_page, tag);
+  console.log('Getting nuggets: ', page, per_page, tag);
   const headers = new Headers();
   headers.append('api-key', process.env.DEV_TO_TOKEN);
 
@@ -94,7 +72,10 @@ const getNuggetsWithTag = async ({ page, per_page, tag }) => {
   let nuggets: Nugget[] = await response.json();
 
   nuggets = nuggets.filter((nugget) => {
-    return nugget.tag_list.includes(tag) && nugget.tag_list.includes('nuggets');
+    return (
+      nugget.tag_list.includes('nuggets') &&
+      (tag ? nugget.tag_list.includes(tag) : true)
+    );
   });
 
   const start = (+page - 1) * +per_page;
@@ -104,33 +85,4 @@ const getNuggetsWithTag = async ({ page, per_page, tag }) => {
   nuggets = nuggets.slice(start, end);
 
   return nuggets;
-};
-
-/**
- * Fetches all the articles published by the given author.
- * @param param of type {@link GetNuggetsProps}.
- */
-const getNuggetsWithoutTag = async ({ page, per_page }) => {
-  console.log('Getting nuggets without tag', page, per_page);
-  const headers = new Headers();
-  headers.append('api-key', process.env.DEV_TO_TOKEN);
-
-  const url = `https://dev.to/api/articles?`;
-
-  const params = new URLSearchParams({
-    page,
-    per_page,
-    tag: 'nuggets',
-    username: 'sidthesloth92'
-  });
-
-  console.log(url + params);
-
-  const response = await fetch(url + params, {
-    headers
-  });
-
-  const nuggets: Nugget[] = await response.json();
-
-  return nuggets;
-};
+}
