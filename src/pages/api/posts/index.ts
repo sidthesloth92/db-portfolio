a11f5e627f5a261f1a0811b1a23c4841cef80cbb
Dinesh Balaji Venkataraj <www.sidthesloth@gmail.com>
2020-12-20 18:09:38 +0530

    fix(posts): fix bug showing nuggests in posts, update sitemap

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -52,40 +52,17 @@ export async function getPosts({
   per_page = process.env.POSTS_PER_PAGE,
   tag = ''
 }: GetPostsProps = {}): Promise<Post[]> {
-  if (tag) {
-    return await getPostsWithTag({
-      page,
-      per_page,
-      tag
-    });
-  }
-
-  return await getPostsWithoutTag({
-    page,
-    per_page
-  });
-}
-
-/**
- * Fetches a list of posts with specified tag. Since DEV.to does not have
- * and endpoint that allows to filter by username and tag, instead we fetch a
- * fairly large number of posts of the user and then try to filter out the posts
- * matching the id.
- * @param param of type {@link GetPostsProps}.
- */
-const getPostsWithTag = async ({ page, per_page, tag }) => {
-  console.log('Getting posts with tag: ', page, per_page, tag);
-  const headers = new Headers();
-  headers.append('api-key', process.env.DEV_TO_TOKEN);
-
   const url = `https://dev.to/api/articles/me/published?`;
 
   const params = new URLSearchParams({
-    page,
+    page: '1',
     per_page: '100'
   });
 
   console.log(url + params);
+
+  const headers = new Headers();
+  headers.append('api-key', process.env.DEV_TO_TOKEN);
 
   const response = await fetch(url + params, {
     headers
@@ -94,7 +71,10 @@ const getPostsWithTag = async ({ page, per_page, tag }) => {
   let posts: Post[] = await response.json();
 
   posts = posts.filter((post) => {
-    return post.tag_list.includes(tag) && !post.tag_list.includes('nuggets');
+    return (
+      (tag ? post.tag_list.includes(tag) : true) &&
+      !post.tag_list.includes('nuggets')
+    );
   });
 
   const start = (+page - 1) * +per_page;
@@ -104,33 +84,4 @@ const getPostsWithTag = async ({ page, per_page, tag }) => {
   posts = posts.slice(start, end);
 
   return posts;
-};
-
-/**
- * Fetches all the articles published by the given author.
- * @param param of type {@link GetPostsProps}.
- */
-const getPostsWithoutTag = async ({ page, per_page }) => {
-  console.log('Getting posts without tag', page, per_page);
-  const headers = new Headers();
-  headers.append('api-key', process.env.DEV_TO_TOKEN);
-
-  const url = `https://dev.to/api/articles?`;
-
-  const params = new URLSearchParams({
-    page,
-    per_page,
-    tags_exclude: 'nuggets',
-    username: 'sidthesloth92'
-  });
-
-  console.log(url + params);
-
-  const response = await fetch(url + params, {
-    headers
-  });
-
-  const posts: Post[] = await response.json();
-
-  return posts;
-};
+}
