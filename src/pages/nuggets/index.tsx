af8caa35069235c2994fbcdc8decbe1170f6793e
karkonium <anandkarki2000@gmail.com>
2021-02-23 16:56:23 -0500

    put nuggets and projects as under construction

--- src/pages/nuggets/index.tsx
+++ src/pages/nuggets/index.tsx
@@ -1,29 +1,36 @@
-import { GetStaticPropsResult } from 'next';
+// import { GetStaticPropsResult } from 'next';
 
+// import withPageTransition from '../../components/hoc/with-page-transition';
+// import { Nugget } from '../../models/Nugget';
+// import NuggetsPage from '../../nuggets/nuggets-page/NuggetsPage.component';
+// import { getNuggets } from '../api/nuggets';
+
+// export interface NuggetsPageProps {
+//   nuggets: Nugget[];
+// }
+
+// export const getStaticProps = async (): Promise<
+//   GetStaticPropsResult<NuggetsPageProps>
+// > => {
+//   console.log('\nRunning getStaticProps for nuggets');
+//   try {
+//     const nuggets = await getNuggets();
+//     return {
+//       props: {
+//         nuggets
+//       },
+//       revalidate: 300
+//     };
+//   } catch (e) {
+//     console.error('Error while fetching getStaticProps nuggets: ', e);
+//   }
+// };
+
+// export default withPageTransition(NuggetsPage);
+
+
+
+import UnderConstruction from '../../landing/components/under-construction/UnderContruction'
 import withPageTransition from '../../components/hoc/with-page-transition';
-import { Nugget } from '../../models/Nugget';
-import NuggetsPage from '../../nuggets/nuggets-page/NuggetsPage.component';
-import { getNuggets } from '../api/nuggets';
 
-export interface NuggetsPageProps {
-  nuggets: Nugget[];
-}
-
-export const getStaticProps = async (): Promise<
-  GetStaticPropsResult<NuggetsPageProps>
-> => {
-  console.log('\nRunning getStaticProps for nuggets');
-  try {
-    const nuggets = await getNuggets();
-    return {
-      props: {
-        nuggets
-      },
-      revalidate: 300
-    };
-  } catch (e) {
-    console.error('Error while fetching getStaticProps nuggets: ', e);
-  }
-};
-
-export default withPageTransition(NuggetsPage);
+export default withPageTransition(UnderConstruction);
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,26 +1,33 @@
-import { GetStaticPropsResult } from 'next';
+// import { GetStaticPropsResult } from 'next';
 
+// import withPageTransition from '../../components/hoc/with-page-transition';
+// import PostsPage, {
+//   PostsPageProps
+// } from '../../posts/posts-page/PostsPage.component';
+// import { getPosts } from '../api/posts';
+
+// export const getStaticProps = async (): Promise<
+//   GetStaticPropsResult<PostsPageProps>
+// > => {
+//   console.log('\nRunning getStaticProps for posts');
+//   try {
+//     const posts = await getPosts();
+//     return {
+//       props: {
+//         posts
+//       },
+//       revalidate: 300
+//     };
+//   } catch (e) {
+//     console.error('Error while fetching getStaticProps posts: ', e);
+//   }
+// };
+
+// export default withPageTransition(PostsPage);
+
+
+
+import UnderConstruction from '../../landing/components/under-construction/UnderContruction'
 import withPageTransition from '../../components/hoc/with-page-transition';
-import PostsPage, {
-  PostsPageProps
-} from '../../posts/posts-page/PostsPage.component';
-import { getPosts } from '../api/posts';
 
-export const getStaticProps = async (): Promise<
-  GetStaticPropsResult<PostsPageProps>
-> => {
-  console.log('\nRunning getStaticProps for posts');
-  try {
-    const posts = await getPosts();
-    return {
-      props: {
-        posts
-      },
-      revalidate: 300
-    };
-  } catch (e) {
-    console.error('Error while fetching getStaticProps posts: ', e);
-  }
-};
-
-export default withPageTransition(PostsPage);
+export default withPageTransition(UnderConstruction);
