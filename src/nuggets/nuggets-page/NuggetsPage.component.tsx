3ad53e8558db6a8259ffeb9a60a5469b2c9b462e
karkonium <anandkarki2000@gmail.com>
2021-02-23 17:18:16 -0500

    commented out unused file

--- src/nuggets/nuggets-page/NuggetsPage.component.tsx
+++ src/nuggets/nuggets-page/NuggetsPage.component.tsx
@@ -1,139 +1,139 @@
-import { motion } from 'framer-motion';
-import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
+// import { motion } from 'framer-motion';
+// import Head from 'next/head';
+// import Link from 'next/link';
+// import { useRouter } from 'next/router';
+// import React from 'react';
+// import InfiniteScroll from 'react-infinite-scroll-component';
 
-import PageBody from '../../components/page-body/PageBody';
-import PageHeader from '../../components/page-header/PageHeader';
-import Page from '../../components/page/Page';
-import { formatDate } from '../../lib';
-import { NuggetsPageProps } from '../../pages/nuggets';
-import { useNuggets } from './NuggetsPage.hooks';
+// import PageBody from '../../components/page-body/PageBody';
+// import PageHeader from '../../components/page-header/PageHeader';
+// import Page from '../../components/page/Page';
+// import { formatDate } from '../../lib';
+// import { NuggetsPageProps } from '../../pages/nuggets';
+// import { useNuggets } from './NuggetsPage.hooks';
 
-/**
- * Animation variants for the table container.
- */
-const tableContainerVariants = {
-  initial: {
-    transform: 'translateY(100%)',
-    opacity: 0
-  },
-  enter: {
-    transform: 'translateY(0px)',
-    opacity: 1,
-    transition: {
-      delay: 0.8,
-      duration: 1,
-      ease: 'easeOut'
-    }
-  }
-};
+// /**
+//  * Animation variants for the table container.
+//  */
+// const tableContainerVariants = {
+//   initial: {
+//     transform: 'translateY(100%)',
+//     opacity: 0
+//   },
+//   enter: {
+//     transform: 'translateY(0px)',
+//     opacity: 1,
+//     transition: {
+//       delay: 0.8,
+//       duration: 1,
+//       ease: 'easeOut'
+//     }
+//   }
+// };
 
-/**
- * The nuggets page of the application.
- */
-const NuggetsPage: React.FC<NuggetsPageProps> = ({
-  nuggets: initialNuggets = []
-}) => {
-  const {
-    query: { tag = '' }
-  } = useRouter();
+// /**
+//  * The nuggets page of the application.
+//  */
+// const NuggetsPage: React.FC<NuggetsPageProps> = ({
+//   nuggets: initialNuggets = []
+// }) => {
+//   const {
+//     query: { tag = '' }
+//   } = useRouter();
 
-  const [nuggets, hasMore, fetchMoreNuggets] = useNuggets(
-    initialNuggets,
-    String(tag)
-  );
+//   const [nuggets, hasMore, fetchMoreNuggets] = useNuggets(
+//     initialNuggets,
+//     String(tag)
+//   );
 
-  return (
-    <Page>
-      <Head>
-        <title>Nuggets {tag && `| ${tag}`}</title>
+//   return (
+//     <Page>
+//       <Head>
+//         <title>Nuggets {tag && `| ${tag}`}</title>
 
-        <meta
-          property="og:title"
-          content={`Nuggets ${tag ? '| - Tiny posts about' + tag : ''}`}
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content={`${process.env.NEXT_PUBLIC_FRONT_END_DOMAIN}/nuggets/tag/${
-            tag ? tag : ''
-          }}`}
-        />
+//         <meta
+//           property="og:title"
+//           content={`Nuggets ${tag ? '| - Tiny posts about' + tag : ''}`}
+//         />
+//         <meta property="og:type" content="website" />
+//         <meta
+//           property="og:url"
+//           content={`${process.env.NEXT_PUBLIC_FRONT_END_DOMAIN}/nuggets/tag/${
+//             tag ? tag : ''
+//           }}`}
+//         />
 
-        <meta
-          name="twitter:title"
-          content={`Nuggets ${tag ? '| - Tiny posts about' + tag : ''}`}
-        />
-        <meta
-          name="twitter:description"
-          content="Tiny bite-sized posts that teach you something new without taking much time of yours."
-        />
-      </Head>
-      <PageHeader
-        title="Nuggets"
-        description="Tiny bite-sized posts that teach you something new without taking much time of yours."
-      />
-      <PageBody>
-        <motion.div variants={tableContainerVariants}>
-          <div className="hidden md:block">
-            <div className="flex flex-wrap w-full text-primary border-b border-dark-light">
-              <div className="w-3/12 p-4 pb-8">Name</div>
-              <div className="w-5/12 p-4 pb-8">Description</div>
-              <div className="w-2/12 p-4 pb-8">Category</div>
-              <div className="w-2/12 p-4 pb-8">Updated</div>
-            </div>
-          </div>
+//         <meta
+//           name="twitter:title"
+//           content={`Nuggets ${tag ? '| - Tiny posts about' + tag : ''}`}
+//         />
+//         <meta
+//           name="twitter:description"
+//           content="Tiny bite-sized posts that teach you something new without taking much time of yours."
+//         />
+//       </Head>
+//       <PageHeader
+//         title="Nuggets"
+//         description="Tiny bite-sized posts that teach you something new without taking much time of yours."
+//       />
+//       <PageBody>
+//         <motion.div variants={tableContainerVariants}>
+//           <div className="hidden md:block">
+//             <div className="flex flex-wrap w-full text-primary border-b border-dark-light">
+//               <div className="w-3/12 p-4 pb-8">Name</div>
+//               <div className="w-5/12 p-4 pb-8">Description</div>
+//               <div className="w-2/12 p-4 pb-8">Category</div>
+//               <div className="w-2/12 p-4 pb-8">Updated</div>
+//             </div>
+//           </div>
 
-          <InfiniteScroll
-            dataLength={nuggets.length}
-            next={fetchMoreNuggets}
-            hasMore={hasMore}
-            loader={null}>
-            {nuggets &&
-              nuggets.map((nugget, index) => {
-                return (
-                  <div
-                    className="flex flex-wrap w-full items-center md:flex-row bg-dark-tint md:bg-dark p-2 md:p-0 my-6 md:my-0 rounded border-b-2 border-secondary-text md:border-b md:border-dark-light"
-                    key={index}>
-                    <div className="w-full md:w-3/12 p-2 md:py-4 md:px-2 lg:p-4 text-secondary-text md:md:text-primary font-bold text-lg">
-                      <Link
-                        href="/nuggets/[id]/[slug]"
-                        as={`/nuggets/${nugget.id}/${nugget.slug}`}>
-                        <a className="ul-hover-effect">{nugget.title}</a>
-                      </Link>
-                    </div>
-                    <div className="w-full md:w-5/12 pb-2 px-2 md:py-4 md:px-2 lg:p-4">
-                      {nugget.description}
-                    </div>
-                    <div className="w-2/3 md:w-2/12 pb-2 px-2 md:py-4 md:px-2 lg:p-4 text-sm text-secondary font-bold  md:text-center">
-                      {nugget.tag_list &&
-                        nugget.tag_list.map((tag) => {
-                          return (
-                            <Link
-                              key={tag}
-                              href="/nuggets/tag/[tag]"
-                              as={`/nuggets/tag/${tag}`}>
-                              <a className="inline-block ul-hover-effect mr-2">
-                                #{tag}
-                              </a>
-                            </Link>
-                          );
-                        })}
-                    </div>
-                    <div className="w-1/3 md:w-2/12 pb-2 px-2 md:py-4 md:px-2 lg:p-4 text-sm text-secondary md:text-white md:text-base font-bold md:font-normal md:text-center">
-                      {formatDate(nugget.published_timestamp)}
-                    </div>
-                  </div>
-                );
-              })}
-          </InfiniteScroll>
-        </motion.div>
-      </PageBody>
-    </Page>
-  );
-};
+//           <InfiniteScroll
+//             dataLength={nuggets.length}
+//             next={fetchMoreNuggets}
+//             hasMore={hasMore}
+//             loader={null}>
+//             {nuggets &&
+//               nuggets.map((nugget, index) => {
+//                 return (
+//                   <div
+//                     className="flex flex-wrap w-full items-center md:flex-row bg-dark-tint md:bg-dark p-2 md:p-0 my-6 md:my-0 rounded border-b-2 border-secondary-text md:border-b md:border-dark-light"
+//                     key={index}>
+//                     <div className="w-full md:w-3/12 p-2 md:py-4 md:px-2 lg:p-4 text-secondary-text md:md:text-primary font-bold text-lg">
+//                       <Link
+//                         href="/nuggets/[id]/[slug]"
+//                         as={`/nuggets/${nugget.id}/${nugget.slug}`}>
+//                         <a className="ul-hover-effect">{nugget.title}</a>
+//                       </Link>
+//                     </div>
+//                     <div className="w-full md:w-5/12 pb-2 px-2 md:py-4 md:px-2 lg:p-4">
+//                       {nugget.description}
+//                     </div>
+//                     <div className="w-2/3 md:w-2/12 pb-2 px-2 md:py-4 md:px-2 lg:p-4 text-sm text-secondary font-bold  md:text-center">
+//                       {nugget.tag_list &&
+//                         nugget.tag_list.map((tag) => {
+//                           return (
+//                             <Link
+//                               key={tag}
+//                               href="/nuggets/tag/[tag]"
+//                               as={`/nuggets/tag/${tag}`}>
+//                               <a className="inline-block ul-hover-effect mr-2">
+//                                 #{tag}
+//                               </a>
+//                             </Link>
+//                           );
+//                         })}
+//                     </div>
+//                     <div className="w-1/3 md:w-2/12 pb-2 px-2 md:py-4 md:px-2 lg:p-4 text-sm text-secondary md:text-white md:text-base font-bold md:font-normal md:text-center">
+//                       {formatDate(nugget.published_timestamp)}
+//                     </div>
+//                   </div>
+//                 );
+//               })}
+//           </InfiniteScroll>
+//         </motion.div>
+//       </PageBody>
+//     </Page>
+//   );
+// };
 
-export default NuggetsPage;
+// export default NuggetsPage;
--- src/pages/nuggets/[id]/[slug].tsx
+++ src/pages/nuggets/[id]/[slug].tsx
@@ -1,229 +1,229 @@
-import { motion } from 'framer-motion';
-import {
-  GetStaticPaths,
-  GetStaticPathsResult,
-  GetStaticPropsResult
-} from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
-import React from 'react';
+// import { motion } from 'framer-motion';
+// import {
+//   GetStaticPaths,
+//   GetStaticPathsResult,
+//   GetStaticPropsResult
+// } from 'next';
+// import Head from 'next/head';
+// import Link from 'next/link';
+// import React from 'react';
 
-import withPageTransition from '../../../components/hoc/with-page-transition';
-import MdContent from '../../../components/md-content/MdContent';
-import PageBody from '../../../components/page-body/PageBody';
-import Page from '../../../components/page/Page';
-import SocialShare from '../../../components/social-share/SocialShare.component';
-import { formatDate } from '../../../lib';
-import { Nugget } from '../../../models/Nugget';
-import ShareSection from '../../../posts/share-section/ShareSection.component';
-import { getNuggets } from '../../api/nuggets';
-import { getNuggetById } from '../../api/nuggets/[id]';
+// import withPageTransition from '../../../components/hoc/with-page-transition';
+// import MdContent from '../../../components/md-content/MdContent';
+// import PageBody from '../../../components/page-body/PageBody';
+// import Page from '../../../components/page/Page';
+// import SocialShare from '../../../components/social-share/SocialShare.component';
+// import { formatDate } from '../../../lib';
+// import { Nugget } from '../../../models/Nugget';
+// import ShareSection from '../../../posts/share-section/ShareSection.component';
+// import { getNuggets } from '../../api/nuggets';
+// import { getNuggetById } from '../../api/nuggets/[id]';
 
-/**
- * Placeholder animation variant for the header section.
- */
-const headerVariants = {
-  initial: {
-    opacity: 0
-  },
-  enter: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.5,
-      when: 'beforeChildren'
-    }
-  }
-};
+// /**
+//  * Placeholder animation variant for the header section.
+//  */
+// const headerVariants = {
+//   initial: {
+//     opacity: 0
+//   },
+//   enter: {
+//     opacity: 1,
+//     transition: {
+//       staggerChildren: 0.5,
+//       when: 'beforeChildren'
+//     }
+//   }
+// };
 
-/**
- * Animation variants for the header children.
- */
-export const headerChildrenVariants = {
-  initial: {
-    opacity: 0,
-    transform: 'translateY(50%)'
-  },
-  enter: {
-    opacity: 1,
-    transform: 'translateY(0%)',
-    transition: {
-      duration: 0.8,
-      ease: 'easeOut'
-    }
-  }
-};
+// /**
+//  * Animation variants for the header children.
+//  */
+// export const headerChildrenVariants = {
+//   initial: {
+//     opacity: 0,
+//     transform: 'translateY(50%)'
+//   },
+//   enter: {
+//     opacity: 1,
+//     transform: 'translateY(0%)',
+//     transition: {
+//       duration: 0.8,
+//       ease: 'easeOut'
+//     }
+//   }
+// };
 
-/**
- * Animation variants for the line after header.
- */
-const hrVariants = {
-  initial: {
-    opacity: 0,
-    scale: 0
-  },
-  enter: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delay: 1.2,
-      duration: 1,
-      ease: 'easeOut'
-    }
-  }
-};
+// /**
+//  * Animation variants for the line after header.
+//  */
+// const hrVariants = {
+//   initial: {
+//     opacity: 0,
+//     scale: 0
+//   },
+//   enter: {
+//     opacity: 1,
+//     scale: 1,
+//     transition: {
+//       delay: 1.2,
+//       duration: 1,
+//       ease: 'easeOut'
+//     }
+//   }
+// };
 
-/**
- * Animation variants for the page content.
- */
-const nuggetContentVariants = {
-  initial: {
-    opacity: 0,
-    transform: 'translateY(2%)'
-  },
-  enter: {
-    opacity: 1,
-    transform: 'translateY(0%)',
-    transition: {
-      delay: 1.2,
-      duration: 1,
-      ease: 'easeOut'
-    }
-  }
-};
+// /**
+//  * Animation variants for the page content.
+//  */
+// const nuggetContentVariants = {
+//   initial: {
+//     opacity: 0,
+//     transform: 'translateY(2%)'
+//   },
+//   enter: {
+//     opacity: 1,
+//     transform: 'translateY(0%)',
+//     transition: {
+//       delay: 1.2,
+//       duration: 1,
+//       ease: 'easeOut'
+//     }
+//   }
+// };
 
-export const getStaticPaths: GetStaticPaths = async (): Promise<
-  GetStaticPathsResult<any>
-> => {
-  try {
-    console.log('\nRunning getStaticPaths for nuggets/[id]/[slug]');
-    const nuggets = await getNuggets({
-      per_page: '50'
-    });
+// export const getStaticPaths: GetStaticPaths = async (): Promise<
+//   GetStaticPathsResult<any>
+// > => {
+//   try {
+//     console.log('\nRunning getStaticPaths for nuggets/[id]/[slug]');
+//     const nuggets = await getNuggets({
+//       per_page: '50'
+//     });
 
-    const paths = nuggets.map((nugget) => ({
-      params: {
-        id: String(nugget.id),
-        slug: nugget.slug
-      }
-    }));
+//     const paths = nuggets.map((nugget) => ({
+//       params: {
+//         id: String(nugget.id),
+//         slug: nugget.slug
+//       }
+//     }));
 
-    console.log('\nNumber of nuggets pre-rendered: ', nuggets.length);
+//     console.log('\nNumber of nuggets pre-rendered: ', nuggets.length);
 
-    return {
-      paths,
-      fallback: true
-    };
-  } catch (e) {
-    console.error('Error while fetching static paths for nuggets: ', e);
-  }
-};
+//     return {
+//       paths,
+//       fallback: true
+//     };
+//   } catch (e) {
+//     console.error('Error while fetching static paths for nuggets: ', e);
+//   }
+// };
 
-/**
- * Props for {@link NuggetPage}.
- */
-interface NuggetPageProps {
-  /**
-   * Nugget for the page.
-   */
-  nugget: Nugget;
-}
+// /**
+//  * Props for {@link NuggetPage}.
+//  */
+// interface NuggetPageProps {
+//   /**
+//    * Nugget for the page.
+//    */
+//   nugget: Nugget;
+// }
 
-export const getStaticProps = async ({
-  params
-}: Record<any, any>): Promise<GetStaticPropsResult<NuggetPageProps>> => {
-  try {
-    console.log('\nRunning getStaticProps for nugget with id: ', params.id);
-    const nugget = await getNuggetById(params.id);
-    nugget.body_markdown = nugget.body_markdown.substring(
-      nugget.body_markdown.lastIndexOf(nugget.description.substring(0, 10))
-    );
-    return {
-      props: {
-        nugget
-      },
-      revalidate: 300
-    };
-  } catch (e) {
-    console.error(
-      'Error while fetching getStaticProps nugget by id: ',
-      params.id,
-      e
-    );
-  }
-};
+// export const getStaticProps = async ({
+//   params
+// }: Record<any, any>): Promise<GetStaticPropsResult<NuggetPageProps>> => {
+//   try {
+//     console.log('\nRunning getStaticProps for nugget with id: ', params.id);
+//     const nugget = await getNuggetById(params.id);
+//     nugget.body_markdown = nugget.body_markdown.substring(
+//       nugget.body_markdown.lastIndexOf(nugget.description.substring(0, 10))
+//     );
+//     return {
+//       props: {
+//         nugget
+//       },
+//       revalidate: 300
+//     };
+//   } catch (e) {
+//     console.error(
+//       'Error while fetching getStaticProps nugget by id: ',
+//       params.id,
+//       e
+//     );
+//   }
+// };
 
-/**
- * Represents a page for a single instance of a nugget.
- */
-const NuggetPage: React.FC<NuggetPageProps> = ({ nugget = {} }) => {
-  return (
-    <Page>
-      <Head>
-        <title>{nugget.title}</title>
-        <meta property="og:title" content={nugget.title} />
-        <meta property="og:type" content="article" />
-        <meta
-          property="og:url"
-          content={`${process.env.NEXT_PUBLIC_FRONT_END_DOMAIN}/nuggets/${nugget.id}/${nugget.slug}`}
-        />
+// /**
+//  * Represents a page for a single instance of a nugget.
+//  */
+// const NuggetPage: React.FC<NuggetPageProps> = ({ nugget = {} }) => {
+//   return (
+//     <Page>
+//       <Head>
+//         <title>{nugget.title}</title>
+//         <meta property="og:title" content={nugget.title} />
+//         <meta property="og:type" content="article" />
+//         <meta
+//           property="og:url"
+//           content={`${process.env.NEXT_PUBLIC_FRONT_END_DOMAIN}/nuggets/${nugget.id}/${nugget.slug}`}
+//         />
 
-        <meta name="twitter:title" content={nugget.title} />
-        <meta name="twitter:description" content={nugget.description} />
-      </Head>
-      <motion.div
-        className="text-center mb-8 lg:mb-16 overflow-hidden"
-        variants={headerVariants}>
-        <motion.h1
-          className="font-black text-primary mb-4 leading-tight"
-          variants={headerChildrenVariants}>
-          {nugget.title}
-        </motion.h1>
-        <motion.div
-          className="font-bold text-lg lg:text-xl text-secondary"
-          variants={headerChildrenVariants}>
-          <span>{formatDate(nugget.published_timestamp)}</span>
-          <span className="px-2">•</span>
-          <span>{nugget.readingTime}</span>
-        </motion.div>
-        <motion.div
-          className="text-secondary-text text-md lg:text-lg mb-2 font-bold"
-          variants={headerChildrenVariants}>
-          {nugget.tag_list &&
-            nugget.tag_list.map((tag) => {
-              return (
-                <Link
-                  key={tag}
-                  href="/nuggets/tag/[tag]"
-                  as={`/nuggets/tag/${tag}`}>
-                  <a className="inline-block ul-hover-effect mr-2">#{tag}</a>
-                </Link>
-              );
-            })}
-        </motion.div>
-        <motion.div
-          className="flex justify-center"
-          variants={headerChildrenVariants}>
-          <SocialShare
-            item={nugget}
-            url={`${process.env.NEXT_PUBLIC_FRONT_END_DOMAIN}/nuggets/${nugget.id}/${nugget.slug}`}
-          />
-        </motion.div>
-      </motion.div>
+//         <meta name="twitter:title" content={nugget.title} />
+//         <meta name="twitter:description" content={nugget.description} />
+//       </Head>
+//       <motion.div
+//         className="text-center mb-8 lg:mb-16 overflow-hidden"
+//         variants={headerVariants}>
+//         <motion.h1
+//           className="font-black text-primary mb-4 leading-tight"
+//           variants={headerChildrenVariants}>
+//           {nugget.title}
+//         </motion.h1>
+//         <motion.div
+//           className="font-bold text-lg lg:text-xl text-secondary"
+//           variants={headerChildrenVariants}>
+//           <span>{formatDate(nugget.published_timestamp)}</span>
+//           <span className="px-2">•</span>
+//           <span>{nugget.readingTime}</span>
+//         </motion.div>
+//         <motion.div
+//           className="text-secondary-text text-md lg:text-lg mb-2 font-bold"
+//           variants={headerChildrenVariants}>
+//           {nugget.tag_list &&
+//             nugget.tag_list.map((tag) => {
+//               return (
+//                 <Link
+//                   key={tag}
+//                   href="/nuggets/tag/[tag]"
+//                   as={`/nuggets/tag/${tag}`}>
+//                   <a className="inline-block ul-hover-effect mr-2">#{tag}</a>
+//                 </Link>
+//               );
+//             })}
+//         </motion.div>
+//         <motion.div
+//           className="flex justify-center"
+//           variants={headerChildrenVariants}>
+//           <SocialShare
+//             item={nugget}
+//             url={`${process.env.NEXT_PUBLIC_FRONT_END_DOMAIN}/nuggets/${nugget.id}/${nugget.slug}`}
+//           />
+//         </motion.div>
+//       </motion.div>
 
-      <PageBody>
-        <motion.div
-          className="bg-dark-light sm: mb-8"
-          style={{ height: '1px' }}
-          variants={hrVariants}
-        />
-        <motion.div variants={nuggetContentVariants}>
-          <MdContent>{nugget.body_markdown}</MdContent>
-        </motion.div>
+//       <PageBody>
+//         <motion.div
+//           className="bg-dark-light sm: mb-8"
+//           style={{ height: '1px' }}
+//           variants={hrVariants}
+//         />
+//         <motion.div variants={nuggetContentVariants}>
+//           <MdContent>{nugget.body_markdown}</MdContent>
+//         </motion.div>
 
-        <ShareSection item={nugget} />
-      </PageBody>
-    </Page>
-  );
-};
+//         <ShareSection item={nugget} />
+//       </PageBody>
+//     </Page>
+//   );
+// };
 
-export default withPageTransition(NuggetPage);
+// export default withPageTransition(NuggetPage);
