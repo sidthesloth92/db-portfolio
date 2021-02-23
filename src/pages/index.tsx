778e915060edd6c551e93d7db96747b7bf8450b8
karkonium <anandkarki2000@gmail.com>
2021-02-23 12:25:47 -0500

    removed the fish help

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,39 +54,6 @@ const Home: React.FC = () => {
         />
       </Head>
       <div className="home-container w-screen h-screen bg-dark-black flex justify-center items-center -mt-20">
-        <motion.div
-          className="fixed top-0 left-0 z-10 m-6"
-          initial="closed"
-          animate={isHelpOpen ? 'open' : 'closed'}>
-          <motion.div
-            className="absolute p-4 transform origin-top-left bg-dark border-2 border-secondary-text select-none"
-            style={{
-              top: '15px',
-              left: '15px',
-              width: '260px'
-            }}
-            variants={helpSectionVariants}>
-            The
-            <span role="img" aria-label="fish" className="ml-1">
-              🐟
-            </span>
-            &apos;s love to be petted! Just drag on the screen and watch them
-            follow you endlessly. They are also always hungry. Double tap to
-            drop some
-            <span role="img" aria-label="food" className="mx-1">
-              🌯
-            </span>
-            and watch them as they gobble it up..{' '}
-            <span role="img" aria-label="yummy">
-              😋
-            </span>
-          </motion.div>
-          <IconHelpCircleFilled
-            className="absolute top-0 left-0 text-3xl cursor-pointer"
-            onClick={() => setIsHelpOpen((isHelpOpen) => !isHelpOpen)}
-          />
-        </motion.div>
-
         <canvas
           className="absolute top-0 left-0 w-full h-full"
           id="landing-canvas"></canvas>
